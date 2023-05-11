8cc1decb9cef04dbcbe0741aeab7c39042aadd36
Mirayne <murainvayne@mail.ru>
2023-05-11 12:43:48 +0300

    added DB connection configuration supply

--- java/ptmk/business/Util.java
+++ java/ptmk/business/Util.java
@@ -1,17 +1,23 @@
 package ptmk.business;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
-    private final static String DB_DRIVER = "org.postgresql.Driver";
-    private final static String DB_USERNAME = "postgres";
-    private final static String DB_PASSWORD = "q1w2e3asd";
-    private final static String DB_URL = "jdbc:postgresql://localhost:5432/PTMK";
+    private static String DB_DRIVER;// = "org.postgresql.Driver";
+    private static String DB_USERNAME;// = "postgres";
+    private static String DB_PASSWORD;// = "12345";
+    private static String DB_URL;// = "jdbc:postgresql://localhost:5432/PTMK";
 
     public Connection getConnection() {
-        //getConnectionInfo();
+        getConnectionInfo();
+
         Connection connection = null;
         try {
             Class.forName(DB_DRIVER);
@@ -21,5 +27,29 @@ public class Util {
             System.out.println("Connection ERROR");
         }
         return connection;
+    }
+
+    private void getConnectionInfo() {
+        List<String> info = getFileRows("connectionInfo.txt");
+        DB_DRIVER = info.get(0).split("=")[1];
+        DB_USERNAME = info.get(1).split("=")[1];
+        DB_PASSWORD = info.get(2).split("=")[1];
+        DB_URL = info.get(3).split("=")[1];
+    }
+
+    private static List<String> getFileRows(String fileName) {
+        List<String> result = new ArrayList<>();
+
+        String currentDir = System.getProperty("user.dir");
+        try (BufferedReader reader = new BufferedReader(new FileReader(currentDir + "/" + fileName))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                result.add(line);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        return result;
     }
 }
